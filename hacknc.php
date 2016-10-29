6bd6cb13b8806d26eddd7ae13549f54e84804bf9
Nadeen Saleh <nsaleh2013@gmail.com>
2016-10-29 17:26:25 -0400

    concat php variable to json

--- hacknc.php
+++ hacknc.php
@@ -5,35 +5,46 @@
 
 <body>
 
+Name: <input type="text" id="input" value="I hate you.">
 
+<button onclick="myFunction()">Display</button>
+<div id="test"></div>
 
-
-
-<form>
-  Text to test:<br>
-  <input type="textbox" name="string: "><br>
-  
-</form>
-
-
+<script>
+function myFunction() {
+    var input = document.getElementById("input").value;
+    document.getElementById('test').innerHTML = input;
+}
+</script>
 
 
 </body>
 
 <?php
+  $strhtml = '<!doctype html>';
+  $dochtml = new DOMDocument();
+  $dochtml -> loadHTML($strhtml);
+  $input = $dochtml->getElementById('input');
+  $input = "I hate you";
 
-include('Requests.php');
-Requests::register_autoloader();
-$headers = array(
-'Content-Type' => 'application/json'
-);
-$data = '{"text": "I hate you"}';
-$options = array('auth' => array('604e4b67-8d17-4838-8011-ca147f3e2d85', 'E4oX5bHo0mZB'));
-$response = Requests::post('https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19', $headers, $data, $options);
+  include('Requests.php');
+  Requests::register_autoloader();
+  $headers = array(
+  'Content-Type' => 'application/json'
+  );
+  $data = '{"text": "'.json_encode($input).'"}';
+
+  $json = array('text' => $input);
+  $json_data = json_encode($json);
 
 
-echo json_encode($response);
+  $options = array('auth' => array('604e4b67-8d17-4838-8011-ca147f3e2d85', 'E4oX5bHo0mZB'));
+  $response = Requests::post('https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19', $headers, $json_data, $options);
+
+
+  echo json_encode($response);
 ?>
+
 
 
 
