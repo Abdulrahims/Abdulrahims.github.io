4efb63e7d8fbcbee6319e145993b9c8f929a15a6
Nadeen Saleh <salehn123@gmail.com>
2016-10-29 13:48:31 -0400

    took out comments

--- T0021-airpair-angularjs-tutorial/js/main.js
+++ T0021-airpair-angularjs-tutorial/js/main.js
@@ -1,11 +1,3 @@
-/**
- * AngularJS Tutorial 1
- * @author Nick Kaye <nick.c.kaye@gmail.com>
- */
-
-/**
- * Main AngularJS Web Application
- */
 var app = angular.module('tutorialWebApp', [
   'ngRoute'
 ]);
@@ -52,46 +44,4 @@ app.controller('PageCtrl', function (/* $scope, $location, $http */) {
   $('.tooltip-social').tooltip({
     selector: "a[data-toggle=tooltip]"
   })
-});
-
-// Progress bar
-angular.module('ui.bootstrap.demo').controller('ProgressDemoCtrl', function ($scope) {
-  $scope.max = 200;
-
-  $scope.random = function() {
-    var value = Math.floor(Math.random() * 100 + 1);
-    var type;
-
-    if (value < 25) {
-      type = 'success';
-    } else if (value < 50) {
-      type = 'info';
-    } else if (value < 75) {
-      type = 'warning';
-    } else {
-      type = 'danger';
-    }
-
-    $scope.showWarning = type === 'danger' || type === 'warning';
-
-    $scope.dynamic = value;
-    $scope.type = type;
-  };
-
-  $scope.random();
-
-  $scope.randomStacked = function() {
-    $scope.stacked = [];
-    var types = ['success', 'info', 'warning', 'danger'];
-
-    for (var i = 0, n = Math.floor(Math.random() * 4 + 1); i < n; i++) {
-        var index = Math.floor(Math.random() * 4);
-        $scope.stacked.push({
-          value: Math.floor(Math.random() * 30 + 1),
-          type: types[index]
-        });
-    }
-  };
-
-  $scope.randomStacked();
 });
